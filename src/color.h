18303d79c1e73fedd631b36f6bb993f9ba16aa0e
Angel Pena <theangel3535@gmail.com>
2025-05-28 00:47:17 -0400

    add:gamma correction

--- src/color.h
+++ src/color.h
@@ -1,16 +1,31 @@
 #pragma once
 
+#include <random>
 #include <util.hpp>
+
+inline f64 LinearToGamma(f64 linear_component){
+  if (linear_component > 0) {
+    return std::sqrt(linear_component);
+  }
+  return 0;
+}
 
 inline std::ostream &operator<<(std::ostream &stream,
                                 const color &pixel_color) {
 
+  color temp_color(
+      LinearToGamma(pixel_color.r),
+      LinearToGamma(pixel_color.g),
+      LinearToGamma(pixel_color.b)
+      );
+
   static const Interval intensity(0.000, 0.999);
   icolor icolor(
-      i32(256 * intensity.clamp(pixel_color.r)),
-      i32(256 * intensity.clamp(pixel_color.g)),
-      i32(256 * intensity.clamp(pixel_color.b))
+      i32(256 * intensity.clamp(temp_color.r)),
+      i32(256 * intensity.clamp(temp_color.g)),
+      i32(256 * intensity.clamp(temp_color.b))
   );
+
 
   stream << icolor.r << ' ' << icolor.g << ' ' << icolor.b << '\n';
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char *argv[]) {
   world.add(std::make_shared<Sphere>(point3(0,0,-1),0.5));
   world.add(std::make_shared<Sphere>(point3(1,0,-1),0.5));
   world.add(std::make_shared<Sphere>(point3(-1,0,-1),0.5));
-  world.add(std::make_shared<Sphere>(point3(-1.7,0,-0.5),0.5));
   world.add(std::make_shared<Sphere>(point3(3,2,-3),0.5));
   world.add(std::make_shared<Sphere>(point3(-3,1,-3),0.7));
   world.add(std::make_shared<Sphere>(point3(0,-100.5,-1),100));
