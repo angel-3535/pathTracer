24db0f99ca3a0ef631e30700f31fa28966dbb034
Angel Pena <theangel3535@gmail.com>
2025-05-28 02:59:06 -0400

    change render scene

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,34 @@
 int main(int argc, char *argv[]) {
   //World
   HittableList world;
-  auto material_ground = std::make_shared<Lambertian>(color(0.8,0.8,0));
-  auto material_center = std::make_shared<Lambertian>(color(0.1,0.2,0.5));
-  auto material_left = std::make_shared<Metal>(color(0.8,0.8,0.8));
-  auto material_right = std::make_shared<Metal>(color(0.8,0.6,0.2));
+  auto material_ground =  std::make_shared<Lambertian>(color(0.8,0.8,0));
+  auto mate_red =         std::make_shared<Lambertian>(color(1.0,0.0,0.0));
+  auto mate_green =       std::make_shared<Lambertian>(color(0.0,1.0,0.0));
+  auto mate_blue =        std::make_shared<Lambertian>(color(0.0,0.0,1.0));
+  auto metal_gray =       std::make_shared<Metal>(color(0.8,0.8,0.8));
+  auto metal_white =       std::make_shared<Metal>(color(1));
+  auto metal_red =        std::make_shared<Metal>(color(1.0,0.1,0.1));
+  auto metal_green =      std::make_shared<Metal>(color(0.1,1.0,0.1));
+  auto metal_blue =       std::make_shared<Metal>(color(0.1,0.1,1.0));
 
-  world.add(std::make_shared<Sphere>(point3(0,0,-1.2),0.5, material_center));
-  world.add(std::make_shared<Sphere>(point3(1,0,-1),0.5, material_right));
-  world.add(std::make_shared<Sphere>(point3(-1,0,-1),0.5, material_left));
-  world.add(std::make_shared<Sphere>(point3(3,2,-3),0.5, material_center));
-  world.add(std::make_shared<Sphere>(point3(-3,1,-3),0.7, material_center));
+  world.add(std::make_shared<Sphere>(point3(0,0,-1.2)     ,0.5, mate_blue));
+  world.add(std::make_shared<Sphere>(point3(1.2,0,0)      ,0.5, mate_red));
+  world.add(std::make_shared<Sphere>(point3(-3,1,-3)      ,0.5, mate_red));
+
+  world.add(std::make_shared<Sphere>(point3(0,  -.4,0.2)  ,0.1, mate_red));
+  world.add(std::make_shared<Sphere>(point3(0.3,-.4,0.2)  ,0.1, mate_green));
+  world.add(std::make_shared<Sphere>(point3(-.3,-.4,0.2)  ,0.1, mate_blue));
+
+  world.add(std::make_shared<Sphere>(point3(-1,0,-1)    ,0.5, metal_red));
+  world.add(std::make_shared<Sphere>(point3(-1.2,0,0)   ,0.5, metal_green));
+  world.add(std::make_shared<Sphere>(point3(1,1.5,-1)   ,0.7, metal_gray));
+  world.add(std::make_shared<Sphere>(point3(1,0,-1)     ,0.5, metal_white));
+
   world.add(std::make_shared<Sphere>(point3(0,-100.5,-1),100, material_ground));
 
   Camera cam;
   cam.aspect_ratio = 16.0 / 9.0;
-  cam.image_width = 1920;
+  cam.image_width = 1280;
   cam.samples_per_pixel = 100;
   cam.max_depth = 50;
 
