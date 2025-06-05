95e48395a6f50dda0653e7581662ef34e4c2a924
Angel Pena <theangel3535@gmail.com>
2025-06-04 22:10:05 -0400

    add camera variable position

--- src/camera.h
+++ src/camera.h
@@ -1,16 +1,23 @@
 #pragma once
+#include "glm/geometric.hpp"
 #include "hittable.h"
 #include "material.h"
-#include "util.hpp"
+#include "util/types.hpp"
+#include "util/util.hpp"
+#include <cmath>
 #include <color.h>
-
 
 class Camera {
 public:
-  f64 aspect_ratio     {1.0};
-  i32 image_width      {100};
+  f64 aspect_ratio{1.0};
+  i32 image_width{100};
   i32 samples_per_pixel{10};
-  i32 max_depth        {10};
+  i32 max_depth{10};
+
+  f64 vfov{90};
+  point3 lookfrom = point3(0);
+  point3 lookat = point3(0, 0, -1);
+  vec3 vup = vec3(0, 1, 0);
 
 private:
   i32 image_height;
@@ -19,6 +26,7 @@ private:
   point3 pixel00_loc;
   vec3 pixel_delta_u;
   vec3 pixel_delta_v;
+  vec3 u, v, w;
 
 public:
   Camera() = default;
@@ -32,10 +40,10 @@ public:
       std::clog << "\rScanlines remaining: " << (image_height - h) << ' '
                 << std::flush;
       for (u32 w = 0; w < image_width; w++) {
-        color pixel_color(0,0,0);
-        for(u32 sample = 0; sample< samples_per_pixel; sample++){
+        color pixel_color(0, 0, 0);
+        for (u32 sample = 0; sample < samples_per_pixel; sample++) {
           Ray r = GetRay(w, h);
-          pixel_color += RayColor(r,max_depth, world);
+          pixel_color += RayColor(r, max_depth, world);
         }
         std::cout << pixel_samples_scale * pixel_color;
       }
@@ -49,30 +57,35 @@ private:
     image_height = i32(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
     pixel_samples_scale = 1.0 / samples_per_pixel;
-    center = point3(0,0,1.0);
+    center = lookfrom;
 
     // viewport dimensions
-    f64 focal_length{1.0};
-    f64 viewport_height{2.0};
+    f64 focal_length{glm::length(lookfrom - lookat)};
+    f64 theta{DegreeToRadians(vfov)};
+    f64 h{std::tan(theta / 2.0)};
+    f64 viewport_height{2.0 * h * focal_length};
     f64 viewport_width{viewport_height * (static_cast<f64>(image_width) /
                                           static_cast<f64>(image_height))};
 
-    vec3 viewport_u(viewport_width, 0.0, 0.0);
-    vec3 viewport_v(0.0, -viewport_height, 0.0);
+    w = normalize(lookfrom - lookat);
+    u = normalize(glm::cross(vup, w));
+    v = glm::cross(w, u);
+
+    vec3 viewport_u(viewport_width * u);
+    vec3 viewport_v(viewport_height * -v);
 
     pixel_delta_u = viewport_u / static_cast<f64>(image_width);
     pixel_delta_v = viewport_v / static_cast<f64>(image_height);
 
-    vec3 viewport_upper_left = center - vec3(0.0, 0.0, focal_length) -
+    vec3 viewport_upper_left = center -(focal_length * w) -
                                (viewport_u / 2.0) - (viewport_v / 2.0);
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
   }
 
   Ray GetRay(i32 i, i32 j) const {
     vec3 offset = SampleSquare();
-    point3 pixel_sample = pixel00_loc 
-      + ((i + offset.x) * pixel_delta_u)
-      + ((j + offset.y) * pixel_delta_v);
+    point3 pixel_sample = pixel00_loc + ((i + offset.x) * pixel_delta_u) +
+                          ((j + offset.y) * pixel_delta_v);
 
     point3 ray_origin = center;
     vec3 ray_direction = pixel_sample - ray_origin;
@@ -82,7 +95,7 @@ private:
     return vec3(Randomf64() - 0.5, Randomf64() - 0.5, 0);
   }
 
-  color RayColor(const Ray &r,i32 depth, const Hittable &world) const {
+  color RayColor(const Ray &r, i32 depth, const Hittable &world) const {
     if (depth <= 0) {
       return color(0);
     }
@@ -90,8 +103,8 @@ private:
     if (world.Hit(r, Interval(0.001, infinity), rec)) {
       Ray scattered;
       color attenuation;
-      if(rec.mat->scatter(r, rec,attenuation,scattered))
-        return attenuation * RayColor(scattered, depth-1, world);
+      if (rec.mat->scatter(r, rec, attenuation, scattered))
+        return attenuation * RayColor(scattered, depth - 1, world);
       return color(0);
     }
 
@@ -99,7 +112,4 @@ private:
     f64 a = 0.5 * (unit_dir.y + 1.0);
     return (1.0 - a) * color(1.0) + a * color(0.5, 0.7, 1.0);
   }
-
-
-
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
-#include "types.hpp"
+#include "util/types.hpp"
 #include <memory>
 #include <util/util.hpp>
 
@@ -15,21 +15,21 @@ int main(int argc, char *argv[]) {
   auto mate_green =       std::make_shared<Lambertian>(color(0.0,1.0,0.0));
   auto mate_blue =        std::make_shared<Lambertian>(color(0.0,0.0,1.0));
   auto metal_gray =       std::make_shared<Metal>(color(0.8,0.8,0.8));
-  auto metal_white =       std::make_shared<Metal>(color(1));
+  auto metal_white =      std::make_shared<Metal>(color(1));
   auto metal_red =        std::make_shared<Metal>(color(1.0,0.1,0.1));
   auto metal_green =      std::make_shared<Metal>(color(0.1,1.0,0.1));
   auto metal_blue =       std::make_shared<Metal>(color(0.1,0.1,1.0));
+  auto glass =            std::make_shared<Dielectric>(1.75);
 
-  world.add(std::make_shared<Sphere>(point3(0,0,-1.2)     ,0.5, mate_blue));
-  world.add(std::make_shared<Sphere>(point3(1.2,0,0)      ,0.5, mate_red));
-  world.add(std::make_shared<Sphere>(point3(-3,1,-3)      ,0.5, mate_red));
+  world.add(std::make_shared<Sphere>(point3(0,0,-1.2)     ,0.5, glass));
+  world.add(std::make_shared<Sphere>(point3(1.2,0,0)      ,0.5, mate_blue));
 
-  world.add(std::make_shared<Sphere>(point3(0,  -.4,0.2)  ,0.1, mate_red));
-  world.add(std::make_shared<Sphere>(point3(0.3,-.4,0.2)  ,0.1, mate_green));
-  world.add(std::make_shared<Sphere>(point3(-.3,-.4,0.2)  ,0.1, mate_blue));
+  world.add(std::make_shared<Sphere>(point3(0,  -.4,-2.2)  ,0.1, mate_red));
+  world.add(std::make_shared<Sphere>(point3(0.3,-.4,-2.2)  ,0.1, mate_green));
+  world.add(std::make_shared<Sphere>(point3(-.3,-.4,-2.2)  ,0.1, mate_blue));
 
-  world.add(std::make_shared<Sphere>(point3(-1,0,-1)    ,0.5, metal_red));
-  world.add(std::make_shared<Sphere>(point3(-1.2,0,0)   ,0.5, metal_green));
+  world.add(std::make_shared<Sphere>(point3(-1,0,-2)    ,0.5, metal_red));
+  world.add(std::make_shared<Sphere>(point3(-2.2,0,0)   ,0.5, metal_green));
   world.add(std::make_shared<Sphere>(point3(1,1.5,-1)   ,0.7, metal_gray));
   world.add(std::make_shared<Sphere>(point3(1,0,-1)     ,0.5, metal_white));
 
@@ -39,7 +39,13 @@ int main(int argc, char *argv[]) {
   cam.aspect_ratio = 16.0 / 9.0;
   cam.image_width = 1280;
   cam.samples_per_pixel = 100;
-  cam.max_depth = 50;
+  cam.max_depth = 100;
+
+
+  cam.vfov = 65;
+  cam.lookfrom = point3(-3,2,1);
+  cam.lookat   = point3(0, 0, -1);
+
 
   cam.render(world);
   return 0;
